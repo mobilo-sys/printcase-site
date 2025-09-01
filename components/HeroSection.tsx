122a41ca2ff878ac2029dc715e7dcc0846a8c739
mobilo-sys <lv.mobilo@gmail.com>
2025-09-01 13:18:24 +0300

    finddevice

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -57,7 +57,7 @@ export default function HeroSection() {
               href="#locations"
               className="btn-hover bg-white text-[#0b388a] px-4 sm:px-6 md:px-8 lg:px-10 py-2 sm:py-3 md:py-4 rounded-full hover:bg-gray-200 transition-colors text-sm sm:text-base md:text-lg lg:text-xl font-black font-sans tracking-wide uppercase"
             >
-              {t("findDevice")}
+              {t("findLocationButton")}
             </Link>
           </div>
         </div>
