f092bf12d661276a5285e3ecc08fa7551ae2fa6f
mobilo-sys <lv.mobilo@gmail.com>
2025-09-03 10:20:09 +0300

    components/icons/index.tsx

--- components/icons/index.tsx
+++ components/icons/index.tsx
@@ -1,3 +1,4 @@
+"use client"
 // FILE: components/icons/index.tsx
 // This file will store all our SVG icon components.
 
@@ -12,7 +13,13 @@ export const PhoneIcon = () => (
 
 export const UploadIcon = () => (
   <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4" stroke="#0B388A" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+    <path
+      d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"
+      stroke="#0B388A"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
     <polyline points="7,10 12,15 17,10" stroke="#0B388A" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
     <line x1="12" y1="15" x2="12" y2="3" stroke="#0B388A" strokeWidth="2" />
   </svg>
@@ -28,7 +35,12 @@ export const PaymentIcon = () => (
 export const CheckIcon = () => (
   <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
     <polyline points="20,6 9,17 4,12" stroke="#0B388A" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-    <path d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10z" stroke="#0B388A" strokeWidth="2" fill="none" />
+    <path
+      d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10z"
+      stroke="#0B388A"
+      strokeWidth="2"
+      fill="none"
+    />
   </svg>
 );
 
@@ -41,14 +53,24 @@ export const LocationIcon = () => (
 
 export const EmailIcon = () => (
   <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z" stroke="#0B388A" strokeWidth="2" fill="none" />
+    <path
+      d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"
+      stroke="#0B388A"
+      strokeWidth="2"
+      fill="none"
+    />
     <polyline points="22,6 12,13 2,6" stroke="#0B388A" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
   </svg>
 );
 
 export const PhoneContactIcon = () => (
   <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z" stroke="#0B388A" strokeWidth="2" fill="none" />
+    <path
+      d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"
+      stroke="#0B388A"
+      strokeWidth="2"
+      fill="none"
+    />
   </svg>
 );
 
